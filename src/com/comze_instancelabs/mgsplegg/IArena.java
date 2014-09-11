6b475583355797b34ebf9da2d20ccc71f1d5e05a
InstanceLabs <danielratke97@hotmail.de>
2014-09-11 12:42:22 +0200

    Cleanup

--- src/com/comze_instancelabs/mgsplegg/IArena.java
+++ src/com/comze_instancelabs/mgsplegg/IArena.java
@@ -15,7 +15,6 @@ import com.comze_instancelabs.minigamesapi.Arena;
 import com.comze_instancelabs.minigamesapi.ArenaType;
 import com.comze_instancelabs.minigamesapi.MinigamesAPI;
 import com.comze_instancelabs.minigamesapi.PluginInstance;
-import com.comze_instancelabs.minigamesapi.util.PowerupUtil;
 import com.comze_instancelabs.minigamesapi.util.Util;
 import com.comze_instancelabs.minigamesapi.util.Validator;
 
@@ -33,24 +32,6 @@ public class IArena extends Arena {
 		this.m = m;
 		MinigamesAPI.getAPI().pinstances.get(m).getArenaListener().loseY = 20;
 	}
-
-	/*
-	 * public void generateArena() {
-	 * 
-	 * Location start = Util.getComponentForArena(m, this.getName(), "area.point0"); Location end = Util.getComponentForArena(m, this.getName(),
-	 * "area.point1");
-	 * 
-	 * int x = Math.min(start.getBlockX(), end.getBlockX()); int y = Math.min(start.getBlockY(), end.getBlockY()); int z = Math.min(start.getBlockZ(),
-	 * end.getBlockZ());
-	 * 
-	 * int x2 = Math.max(start.getBlockX(), end.getBlockX()); int y2 = Math.max(start.getBlockY(), end.getBlockY()); int z2 =
-	 * Math.max(start.getBlockZ(), end.getBlockZ());
-	 * 
-	 * for (int i = 0; i < (x2 - x); i++) { for (int j = 0; j <= (y2 - y); j++) { for (int k = 0; k < (z2 - z); k++) { Block b =
-	 * start.getWorld().getBlockAt(new Location(start.getWorld(), x + i, y + j, z + k)); b.setType(Material.SNOW_BLOCK); } } }
-	 * 
-	 * }
-	 */
 
 	@Override
 	public void start(boolean tp) {
@@ -75,7 +56,7 @@ public class IArena extends Arena {
 					try {
 						Player p = Bukkit.getPlayer(a.getAllPlayers().get((int) Math.random() * (a.getAllPlayers().size() - 1)));
 						if (p != null) {
-							PowerupUtil.spawnPowerup(p.getLocation().clone().add(0D, 5D, 0D), getItemStack());
+							Util.spawnPowerup(m, a, p.getLocation().clone().add(0D, 5D, 0D), getItemStack());
 						}
 					} catch (Exception e) {
 						System.out.println("Use the latest MinigamesLib version to get powerups.");
